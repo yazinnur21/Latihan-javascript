a08c42636b38b2d97caa3c3223a9920854bb69f8
ZinArchie <yazinnur21@gmail.com>
2025-09-22 23:53:29 +0800

    senin malam di pc

--- Latihan-javascript/Project-3/jika.js
+++ Latihan-javascript/Project-3/jika.js
@@ -26,16 +26,93 @@
 
 // ================================
 
-let nilai = 65;
+// let nilai = 95;
 
-if (nilai >= 90) {
-    console.log("Grade A");
-} else if(nilai >=75) {
-    console.log("Grade B");
-} else {
-    console.log("Grade C");
-}
+// if (nilai >= 90) {
+//     console.log("Grade A");
+// } else if(nilai >=75) {
+//     console.log("Grade B");
+// } else {
+//     console.log("Grade C");
+// }
 
 // ================================
+
+// const kendaraan = {
+//     merek : "suzuki",
+//     kecepatan : 0,
+    
+//     percepatan : function(nilai){
+//         this.kecepatan += nilai ;
+//         console.log(`Kecepatan sekarang : ${this.kecepatan}`)
+//     },
+//     rem : function(nilai){
+//         this.kecepatan -= nilai;
+
+//         if (this.kecepatan <0){
+//             this.kecepatan = 0;
+//         }
+//         console.log(`Kecepatan sekarang : ${this.kecepatan}`)
+//     }
+// };
+
+// kendaraan.percepatan(50);
+// kendaraan.rem(20);
+
+// ================================
+
+// const kendaraan = {
+//     merek : "suzuki",
+//     kecepatan : 0,
+//     bahanbakar : 3,
+
+//     info : function(){
+//         console.log(`Merek : ${this.merek} | kecepatan ${this.kecepatan}`)
+//     },
+    
+//     percepatan : function(nilai){
+//         if (this.bahanbakar > 0) {
+//             this.kecepatan += nilai;
+//             this.bahanbakar -= 1;
+//             console.log(`Gass!! kecepatan ${this.kecepatan} | bahan bakar ${this.bahanbakar}`)
+//         } else{
+//             console.log("Bahan bakar sudah habis, tidak bisa dipercepat");
+//         }
+//     },
+// };
+// kendaraan.info();
+// kendaraan.percepatan(30);
+// kendaraan.percepatan(30);
+// kendaraan.percepatan(30);
+// kendaraan.percepatan(30);
+
+// ================================
+
+let siswa = {
+    "nama" : "budi",
+    "umur" : 15,
+    "kelas" : "11 IPA",
+    "hobi" : ["Membaca", "berenang", "main game"]
+};
+
+console.log("Data Sekarang", siswa);
+console.log(siswa.nama);
+console.log(siswa.hobi);
+
+siswa.alamat = "Jl. Sudiang";
+console.log("Alamat siswa telah ditambahkan", siswa.alamat);
+
+siswa.umur = 19;
+console.log("Umur anda telah berubah : ", siswa.umur);
+
+delete siswa.kelas;
+console.log("Data setelah dihapus", siswa);
+
+let JsonString = JSON.stringify(siswa);
+console.log("Dalam bentuk string json : ", JsonString);
+
+let dataBaru = JSON.parse(JsonString);
+console.log("Kembali ke bentuk Object : ", dataBaru);
+
 
 
